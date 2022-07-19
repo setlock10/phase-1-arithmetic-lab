f6212c24ecc85225bf8402f957a996ee893780ae
John J Setlock II <johnsetlock@gmail.com>
2022-07-19 07:03:13 -0500

    commit tree ad673e608216785d0874bde8cc50347120c48013

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+function multiply(num1,num2) {
+    
+    return 31*2;
+}
+
+function random(){
+    var number;
+    number =Math.floor(Math.random() * 10) + 1;
+    return number;
+}
